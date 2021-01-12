fc5c06a8558b400e280e43023c26a807e4df04eb
peeyush jain <95peeyushjain@gmail.com>
2021-01-12 20:33:36 +0530

    let var and const

---
+++ 01-Fundatamentals-Part-1/letConstVar.js
@@ -0,0 +1,10 @@
+const firstName = "peeyush"
+
+// firstName = "jain" //const variable value are not changeable
+
+// const age; //const variable can't be left uninitialised
+
+lastName = "jain";
+
+console.log(typeof lastName)
+console.log(lastName); //no error bcz in JS we can define variables as in line 7 but not recommended.
