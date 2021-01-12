9901990f078f2d916f7445726676ada0fc0455c4
peeyush jain <95peeyushjain@gmail.com>
2021-01-12 22:20:45 +0530

    basic operators

---
+++ 01-Fundatamentals-Part-1/assignment3.js
@@ -0,0 +1,10 @@
+let myCountryPopulation = 100;
+console.log(myCountryPopulation / 2); //if country divided into 2 parts
+
+myCountryPopulation++;
+console.log(myCountryPopulation); //after inc by 1
+
+console.log(myCountryPopulation < 6)//finland(6 million) has more population or not
+
+let description = "Portugal is in Europe, and its 11 million people speak portuguese";
+console.log(description);
