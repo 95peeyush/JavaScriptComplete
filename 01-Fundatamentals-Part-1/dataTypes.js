4a03302fc9564933dce4cbebd5e8e970bfd7001c
peeyush jain <95peeyushjain@gmail.com>
2021-01-12 20:07:18 +0530

    data types

---
+++ 01-Fundatamentals-Part-1/dataTypes.js
@@ -0,0 +1,19 @@
+
+let isDynamicTyped = true;
+console.log(typeof isDynamicTyped);
+isDynamicTyped = "YES!"; //dynamically typed depending on the type of value data type is decided
+console.log(typeof isDynamicTyped);
+
+let age = 25;
+console.log(typeof age); //number
+
+let firstName = "peeyush";
+console.log(typeof firstName); //string
+
+let lastName;
+
+console.log(typeof lastName); //undefined
+
+
+//bug in JS but it has not be corrected due to legacy reasons
+console.log(typeof null); //object
