05539362dcc99d3df15236bc823a77724ce5b766
peeyush jain <95peeyushjain@gmail.com>
2021-01-13 22:58:39 +0530

    Logical Operator Basic

---
+++ 01-Fundatamentals-Part-1/assignments/assignment9.js
@@ -0,0 +1,8 @@
+const speakEnglish = true;
+const populationLessThan50M = true;
+const isIsland = false;
+
+if (speakEnglish && populationLessThan50M && !isIsland)
+    console.log(`You should live in Portugal :)`);
+else
+    console.log(`Portugal does not meet your criteria :(`);
