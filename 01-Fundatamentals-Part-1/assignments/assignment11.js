61b0a7daa4125bda690af215437b93d6c34ea2b3
peeyush jain <95peeyushjain@gmail.com>
2021-01-14 22:21:53 +0530

    arrays

--- 01-Fundatamentals-Part-1/assignments/assignment11.js
+++
@@ -1,7 +0,0 @@
-function describeCountry(country, population, capitalCity) {
-    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
-}
-
-console.log(describeCountry('India', 1000, 'Delhi'));
-console.log(describeCountry('SriLanka', 100, 'Columbo'));
-console.log(describeCountry('USA', 500, 'New York'));
--- 01-Fundatamentals-Part-1/assignments/assignment13.js
+++
@@ -1,5 +0,0 @@
-let percentageOfWorld3 = population => (population / 79);
-
-console.log(percentageOfWorld3(1220));
-console.log(percentageOfWorld3(400));
-console.log(percentageOfWorld3(591));
