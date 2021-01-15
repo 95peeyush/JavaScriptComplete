1d8398d02fc8ca604c3cf5200d0d96499eb09fad
peeyush jain <95peeyushjain@gmail.com>
2021-01-15 13:49:25 +0530

    objects

---
+++ 02-Fundatamentals-Part-2/assignments/assignment7.js
@@ -0,0 +1,8 @@
+const myCountry = {
+    country: 'India',
+    capital: 'Delhi',
+    language: ['Hindi', 'English', 'Marathi'],
+    population: 1000,
+    neighbours: ['Afghanistan', 'Bangladesh', 'Nepal', 'Sri-Lanka']
+}
+console.log(myCountry);
---
+++ 02-Fundatamentals-Part-2/object.js
@@ -0,0 +1,18 @@
+const peeyushArray = [
+    'peeyush',
+    'jain',
+    2021 - 1995,
+    'Developer',
+    ['sahitya', 'kushagra', 'tarun', 'vicky']
+]
+
+console.log(peeyushArray);
+
+const peeyushObject = { //this type of declaration of object is callled literal object as we literally define the object
+    firstName: 'peeyush',
+    lastName: 'jain',
+    age: 2021 - 1995,
+    job: 'Developer',
+    friends: ['sahitya', 'kushagra', 'tarun', 'vicky']
+}
+console.log(peeyushObject);
