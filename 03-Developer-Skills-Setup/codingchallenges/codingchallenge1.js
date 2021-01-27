217a3ad4e0df0cbc3955cfba00f383d995b70b31
peeyush jain <95peeyushjain@gmail.com>
2021-01-27 15:50:52 +0530

    coding challenge 1

---
+++ 03-Developer-Skills-Setup/codingchallenges/codingchallenge1.js
@@ -0,0 +1,12 @@
+let printForecast = function (temps) {
+  let outputString = "";
+  for (let i = 0; i < temps.length; i++) {
+    outputString += `... ${temps[i]}°C in ${i + 1}days`;
+  }
+  console.log(outputString + `...`);
+};
+
+let arr1 = [17, 21, 23];
+let arr2 = [12, 5, -5, 0, 4];
+printForecast(arr1);
+printForecast(arr2);
